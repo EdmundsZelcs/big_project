249d2f9c9eaf33f402d5bbaa730791c45f63e300
Edmunds <edmunds.zelcs@valmierastehnikums.lv>
2022-11-22 12:16:59 +0200

    big_project

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -7,7 +7,11 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
 </head>
 <style>
 .skats {
--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -1,4 +1,59 @@
 @extends('layout/main')
 @section('content')
+<body>
+{{-- <div class="row">
+    <div class="col-lg-4 offset-4">
+    <input type = "text" id="cityName" placeholder="City name" class="form-control"> 
+    <button class="btn btn-info" id="search"><i class="fas fa-search"></i></button>
+    </div>
+</div>
+    
+    
+<div class="mb-4 pb-2">
+    <div class="card shadow-0">
+        <div class="card-body p-4">
+    
+        <h4 class="mb-1 sfw-normal" id="city"></h4>
+        <h6 class="mb-1 sfw-normal" id="country"></h6>
+        <p class="mb-2" id="temperature"></p>
+        <p id="cloud"></p>
+        <div class="d-flex flex-row align-items-center">
+            <p class="mb-0 me-4" id="cloud"></p>
+        </div>
+    
+    </div>
+</div> --}}
+<script>
+$("#search").on("click", function(){
+    var city = $("#cityName").val();
+    var url = "https://wordsapiv1.p.rapidapi.com/words/hatchback/typeOf"
+    search(url);
+});
 
+$(document).ready(function(){
+    // function search(url){
+        let settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://mashape-community-urban-dictionary.p.rapidapi.com/define",
+        "method": "GET",
+        "headers": {
+            "X-RapidAPI-Key": "'81cab233dfmshe882d2f6381eb5cp134584jsn6b012dfb0de1'",
+            "X-RapidAPI-Host": "mashape-community-urban-dictionary.p.rapidapi.com"
+        }
+        }
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+});
+
+//     $.ajax(settings).done(function (response) {
+//         console.log(response);
+//         $("#city").html(response.location.name)
+//         $("#country").html(response.location.country)
+//         $("#temperature").html("<p>Current temperature: "+response.current.temp_c+" °C</p>")
+//         $("#cloud").html("<p class= mb-0 me-4>Clouds: "+response.current.condition.text+"</p>")
+//     })
+// };
+</script>
 @endsection
